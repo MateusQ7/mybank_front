aa8383e21d93f2b67d5724dc21041fcdcc46d182
MateusQ7 <mateusquixada@gmail.com>
2024-06-29 14:09:30 -0300

    feat: adiciona tudo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 
 export const routes: Routes = [
 
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 
 ];
