883ecd72bb86989cac2ed29b6e8cde41bdf35e99
dante <danted2300@gmail.com>
2024-06-29 12:09:26 -0300

    fix: refatora app component route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+
+];
