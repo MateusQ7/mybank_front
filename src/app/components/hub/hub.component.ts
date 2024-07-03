949f4778ef41ba4581af9ee1c2f3a624c48a5f5d
dante <danted2300@gmail.com>
2024-07-03 17:43:48 -0300

    feat: Adiciona design hub

---
+++ src/app/components/hub/hub.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hub',
+  standalone: true,
+  imports: [],
+  templateUrl: './hub.component.html',
+  styleUrl: './hub.component.css'
+})
+export class HubComponent {
+
+}
