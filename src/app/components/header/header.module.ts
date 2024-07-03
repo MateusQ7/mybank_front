6f14b4a235126b684969c761213e2c5117008022
dante <danted2300@gmail.com>
2024-07-03 14:21:57 -0300

    feat: Adiciona header component ao projeto

---
+++ src/app/components/header/header.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeaderModule { }
