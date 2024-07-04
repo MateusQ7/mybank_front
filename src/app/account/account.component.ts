8245db74a5cb584a0c2fa8a3e85649b292541daa
MateusQ7 <mateusquixada@gmail.com>
2024-07-04 13:52:25 -0300

    feat: Adiciona account component ao projeto

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../components/header/header.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { HomeComponent } from '../home/home.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { HubComponent } from '../components/hub/hub.component';
+
+@Component({
+  selector: 'app-account',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    HubComponent
+  ],
+  templateUrl: './account.component.html',
+  styleUrl: './account.component.css'
+})
+export class AccountComponent {
+
+}
