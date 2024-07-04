2a7963ee1252ec3a2414c1a72b4a9d8ccd3d1bde
dante <danted2300@gmail.com>
2024-07-04 17:25:21 -0300

    fix: Muda o path por causa da alteração do local da pasta home

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
-import { HomeComponent } from '../home/home.component';
+import { HomeComponent } from '../components/home/home.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { HubComponent } from '../components/hub/hub.component';
 
