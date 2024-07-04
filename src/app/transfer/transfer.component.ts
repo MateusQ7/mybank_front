4eb6cf5fd58aac0393db5bf37562ae9b95b32f8f
dante <danted2300@gmail.com>
2024-07-04 17:18:14 -0300

    feate: adiciona transfer compoenents

---
+++ src/app/transfer/transfer.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { HomeComponent } from '../components/home/home.component';
+import { HubComponent } from '../components/hub/hub.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-transfer',
+  standalone: true,
+  imports: [NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    HubComponent,
+    HeaderComponent,
+    RouterModule
+  ],
+  templateUrl: './transfer.component.html',
+  styleUrl: './transfer.component.css'
+})
+export class TransferComponent {
+
+}
