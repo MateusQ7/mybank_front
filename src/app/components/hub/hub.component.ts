f014ae20af56fdce2e560b53fc03028ac478314a
dante <danted2300@gmail.com>
2024-07-12 14:08:01 -0300

    feat: adiciona configdetailsComponent nos imports

--- src/app/components/hub/hub.component.ts
+++ src/app/components/hub/hub.component.ts
@@ -3,13 +3,15 @@ import { DynamicCardComponent } from '../dynamic-card/dynamic-card.component';
 import { AccountDetailsComponent } from '../account-details/account-details.component';
 import { TransferComponent } from '../../pages/transfer/transfer.component';
 import { TransferDetailsComponent } from '../transfer-details/transfer-details.component';
+import { ConfgDetailsComponent } from '../confg-details/confg-details.component';
 
 @Component({
   selector: 'app-hub',
   standalone: true,
   imports: [DynamicCardComponent,
     AccountDetailsComponent,
-    TransferDetailsComponent
+    TransferDetailsComponent,
+    ConfgDetailsComponent
   ],
   templateUrl: './hub.component.html',
   styleUrl: './hub.component.css'
