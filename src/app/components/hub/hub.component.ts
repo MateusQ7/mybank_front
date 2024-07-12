b78f4c394421d37b604ccd346f4d632de7eb7440
MateusQ7 <mateusquixada@gmail.com>
2024-07-12 14:05:45 -0300

    feat: Adiciona invoice-component para o component

--- src/app/components/hub/hub.component.ts
+++ src/app/components/hub/hub.component.ts
@@ -3,13 +3,15 @@ import { DynamicCardComponent } from '../dynamic-card/dynamic-card.component';
 import { AccountDetailsComponent } from '../account-details/account-details.component';
 import { TransferComponent } from '../../pages/transfer/transfer.component';
 import { TransferDetailsComponent } from '../transfer-details/transfer-details.component';
+import { InvoiceDetailsComponent } from '../invoice-details/invoice-details.component';
 
 @Component({
   selector: 'app-hub',
   standalone: true,
   imports: [DynamicCardComponent,
     AccountDetailsComponent,
-    TransferDetailsComponent
+    TransferDetailsComponent,
+    InvoiceDetailsComponent
   ],
   templateUrl: './hub.component.html',
   styleUrl: './hub.component.css'
