d074b4516a73bd4bf07aab169e8caf8fa0e033bb
MateusQ7 <mateusquixada@gmail.com>
2024-07-16 15:53:01 -0300

    feat: Adiciona FormsModule e ReactiveFormsModule ao register.module

--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { RegisterComponent } from './register.component'; // Verifique o caminho correto para o RegisterComponent
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     // Certifique-se de que o RegisterComponent está declarado corretamente aqui
-    RegisterComponent
   ],
   imports: [
     CommonModule,
-    RouterModule // Certifique-se de que RouterModule seja importado corretamente se for necessário para o RegisterComponent
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
-    RegisterComponent // Exporta o RegisterComponent para ser usado em outros módulos
   ]
 })
 export class RegisterModule { }
