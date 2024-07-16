daf1db5697e4d39652ab8747600f75ff428798c3
dante <danted2300@gmail.com>
2024-07-16 13:38:01 -0300

    feat: adiciona pop-up-dialog

--- src/app/components/transfer-details/transfer-details.component.ts
+++ src/app/components/transfer-details/transfer-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { PopUpTransferComponent } from '../pop-up-transfer/pop-up-transfer.component';
 
 @Component({
   selector: 'app-transfer-details',
@@ -8,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './transfer-details.component.css'
 })
 export class TransferDetailsComponent {
+  constructor(public dialog: MatDialog) { }
 
+  openDialog(): void {
+    this.dialog.open(PopUpTransferComponent, {
+      width: '580px',
+      height: '610px',
+      data: { name: 'Angular' },
+      panelClass: 'custom-dialog'
+    })
+    console.log("FUI CLICADO")
+  }
 }
