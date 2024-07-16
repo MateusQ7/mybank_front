5e7d960e1389900242fb243ba81f7ef55191d583
dante <danted2300@gmail.com>
2024-07-16 13:35:03 -0300

    feat: Adiciona pop-up-transfer-compoentens

---
+++ src/app/components/pop-up-transfer/pop-up-transfer.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PopUpTransferComponent } from './pop-up-transfer.component';
+
+describe('PopUpTransferComponent', () => {
+  let component: PopUpTransferComponent;
+  let fixture: ComponentFixture<PopUpTransferComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PopUpTransferComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PopUpTransferComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
