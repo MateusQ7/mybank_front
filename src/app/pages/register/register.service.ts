618bef39579348093e5c2e9c46586c59bd7a4a12
MateusQ7 <mateusquixada@gmail.com>
2024-07-25 13:26:11 -0300

    feat: adiciona registerservice com a lógica do post

--- src/app/pages/register/register.service.ts
+++ src/app/pages/register/register.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ConfigService } from '../../services/config/config.service';
+import { FormatedForm } from '../../shared/models/formatedForm';
+import { Observable } from 'rxjs';
+import { BackResponse } from '../../shared/types/back-response.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private config: ConfigService
+  ) { }
+
+  submit(formData: FormatedForm): Observable<BackResponse>{
+    return this.http.post<BackResponse>(`${this.config.apiUrl}/auth/register`, formData)
+  }
 }
