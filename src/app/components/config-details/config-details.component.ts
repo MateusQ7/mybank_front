e0f494aa852c7e6afafa4364ff8e1500ea2f55db
dante <danted2300@gmail.com>
2024-08-30 18:40:44 -0300

    feat: New features for validation

--- src/app/components/config-details/config-details.component.ts
+++ src/app/components/config-details/config-details.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+// ConfigDetailsComponent
+
+import { Component, OnInit } from '@angular/core';
 import { ConfigDetailsService } from './config-details.service';
-import { FormatedForm } from '../../shared/models/formatedForm';
 import { FormatedFormUser } from '../../shared/models/FormatedFormUser';
 import {
   FormBuilder,
@@ -10,7 +11,6 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
-import { LoginService } from '../../pages/login/login.service';
 import { ToastrService } from 'ngx-toastr';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
@@ -22,7 +22,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   templateUrl: './config-details.component.html',
   styleUrl: './config-details.component.css',
 })
-export class ConfigDetailsComponent {
+export class ConfigDetailsComponent implements OnInit {
   form!: FormGroup;
 
   constructor(
@@ -30,65 +30,71 @@ export class ConfigDetailsComponent {
     private router: Router,
     private configDetailsService: ConfigDetailsService,
     private toastr: ToastrService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      phone: [
-        {
-          value: '',
-          disabled: false,
-        },
-        [Validators.required],
-      ],
-      password: [
-        {
-          value: '',
-          disabled: false,
-        },
-        [Validators.minLength(6), Validators.required],
-      ],
-      confirmPassword: [
-        {
-          value: '',
-          disabled: false,
-        },
-        [Validators.minLength(6), Validators.required],
-      ],
-      email: [
-        {
-          value: '',
-          disabled: false,
-        },
-        [Validators.email, Validators.required],
-      ],
-    });
+    this.form = this.formBuilder.group(
+      {
+        phone: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+      },
+      {
+        validator: this.passwordMatchValidator,
+      }
+    );
+
+    const errorMessage: string | null = sessionStorage.getItem('errorMessage');
+    if (errorMessage) {
+      this.toastr.error(errorMessage);
+      sessionStorage.removeItem('errorMessage');
+    }
+  }
+
+  passwordMatchValidator(form: FormGroup): void {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+
+    if (password && confirmPassword) {
+      if (password.value !== confirmPassword.value) {
+        confirmPassword.setErrors({ mismatch: true });
+      } else {
+        confirmPassword.setErrors(null);
+      }
+    }
   }
 
   updateUser(): void {
+    if (this.form.invalid) {
+      sessionStorage.setItem('errorMessage', 'Por favor, preencha todos os campos corretamente.');
+      window.location.reload();
+      return;
+    }
+
     const storageCpf = sessionStorage.getItem('cpf');
     if (!storageCpf) {
-      this.toastr.error('CPF não encontrado no sessionStorage.');
+      sessionStorage.setItem('errorMessage', 'CPF não encontrado no sessionStorage.');
+      window.location.reload();
       return;
     }
 
     const formatedFormUser: FormatedFormUser = {
       email: this.form.value.email,
       password: this.form.value.password,
-      confirmPassword: this.form.value.confirmPassword, // Corrigido o nome da propriedade
+      confirmPassword: this.form.value.confirmPassword,
       phone: this.form.value.phone,
       cpf: storageCpf,
     };
 
     this.configDetailsService.updateUser(formatedFormUser).subscribe(
       (response) => {
-        // Ajuste o tipo conforme a resposta esperada da API
-        this.toastr.success('Usuário atualizado com sucesso!');
-        console.log(response);
+        this.router.navigate(['/login'], { queryParams: { successMessage: 'Usuário atualizado com sucesso!' } });
       },
       (error) => {
         console.error('Erro ao atualizar usuário', error);
-        this.toastr.error('Erro ao atualizar usuário.');
+        sessionStorage.setItem('errorMessage', 'Erro ao atualizar usuário.');
+        window.location.reload();
       }
     );
   }
