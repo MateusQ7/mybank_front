9318c96c785b0fb91d683d5aae92395283c558ba
dante <danted2300@gmail.com>
2024-09-02 16:11:47 -0300

    feat add new inputs type Cpf and enter description pop-up

--- src/app/components/pop-up-transfer/pop-up-transfer.component.ts
+++ src/app/components/pop-up-transfer/pop-up-transfer.component.ts
@@ -1,19 +1,39 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { PopUpCardsComponent } from '../pop-up-cards/pop-up-cards.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pop-up-transfer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './pop-up-transfer.component.html',
   styleUrl: './pop-up-transfer.component.css'
 })
 export class PopUpTransferComponent {
 
   constructor(public dialogRef: MatDialogRef<PopUpCardsComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
+  selectedOption: string = '';
+  cpf: string = '';
+  transferValue: string = '';
+  description: string = '';
+  transferType: string = '';
+
+  onOptionChange() {
+    if (this.selectedOption !== 'opcao4') {
+      this.cpf = ''; // Limpa o campo CPF se a opção selecionada não for CPF
+    }
+  }
 
   onClose(): void {
     this.dialogRef.close();
   }
+
+  onCreate() {
+    // Lógica para criar a transferência
+  }
+
+
+
 }
---
+++ src/app/components/pop-up-transfer/pop-up-transfer.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { TransactionModel } from '../../shared/models/transactionModel';
+import { catchError, Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ConfigService } from '../../services/config/config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PopUpTransferService {
+
+  constructor(
+    private config: ConfigService,
+    private http: HttpClient
+  ) { }
+
+  createTransaction(transactionModel: TransactionModel): Observable<string> {
+    const token = sessionStorage.getItem('auth-token');
+
+    let headers = new HttpHeaders();
+    if (token) {
+      headers = headers.set('Authorization', `Bearer ${token}`);
+    }
+
+    return this.http.post<string>(`${this.config.apiUrl}/transaction/create`, transactionModel, { headers }).pipe(
+      catchError(error => {
+        console.error('Erro ao criar transação', error);
+        throw error;
+      })
+    );
+  }
+
+}
+
+
