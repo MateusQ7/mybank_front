bdad032277b9e5f12dd476b529f3c68a5ba6f3e0
Dante <danted2300@gmail.com>
2024-10-28 14:29:51 -0300

    feat/Add validation for PopupInvoice(cpf and valueInvoice)

--- src/app/components/pop-up-invoice/pop-up-invoice.component.ts
+++ src/app/components/pop-up-invoice/pop-up-invoice.component.ts
@@ -1,19 +1,43 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
+
 import { ToastrService } from 'ngx-toastr';
 import { PayInvoiceModel } from '../../shared/models/payInvoiceModel';
 import { PopUpInvoiceService } from './pop-up-invoice.service';
+import { InvoiceDetailsService } from '../invoice-details/invoice-details.service';
+import { Invoice } from '../../shared/models/invoiceModel';
 
 @Component({
   selector: 'app-pop-up-invoice',
   standalone: true,
-  imports: [MatDialogModule, FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    NgxMaskDirective,
+    NgxMaskPipe,
+  ],
   templateUrl: './pop-up-invoice.component.html',
-  styleUrls: ['./pop-up-invoice.component.css']
+  styleUrls: ['./pop-up-invoice.component.css'],
 })
 export class PopUpInvoiceComponent implements OnInit {
+  invoices: Invoice[] = [];
+  invoice: Invoice | undefined;
+  error: string | undefined;
   paymentConfirmationForm!: FormGroup;
   currentDate: string | undefined;
 
@@ -22,8 +46,9 @@ export class PopUpInvoiceComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: any,
     private readonly fb: FormBuilder,
     private readonly toastr: ToastrService,
-    private readonly popUpInvoiceService: PopUpInvoiceService
-  ) { }
+    private readonly popUpInvoiceService: PopUpInvoiceService,
+    private readonly invoiceDetailsService: InvoiceDetailsService
+  ) {}
 
   ngOnInit(): void {
     const today = new Date();
@@ -31,7 +56,10 @@ export class PopUpInvoiceComponent implements OnInit {
 
     this.paymentConfirmationForm = this.fb.group({
       cpf: ['', [Validators.required, Validators.pattern('[0-9]{11}')]],
-      paymentValue: ['', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?$')]],
+      paymentValue: [
+        '0.00',
+        [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?$')],
+      ],
       paymentDate: [this.currentDate, Validators.required],
     });
   }
@@ -40,27 +68,97 @@ export class PopUpInvoiceComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  payInvoice(): void {
+  async payInvoice(): Promise<void> {
     if (this.paymentConfirmationForm.invalid) {
-      this.toastr.error('Por favor, preencha todos os campos obrigatórios e verifique se os CPFs são diferentes.');
+      this.toastr.error(
+        'Por favor, preencha todos os campos obrigatórios e verifique se os CPFs são diferentes.'
+      );
       return;
     }
 
-    const payModel: PayInvoiceModel = {
-      accountCpf: this.paymentConfirmationForm.value.cpf,
-      payValue: this.paymentConfirmationForm.value.paymentValue,
-    };
+    const invoice: Invoice | undefined = await this.loadInvoiceByCpf();
 
-    this.popUpInvoiceService.payInvoice(payModel).subscribe({
-      next: (response) => {
-        this.dialogRef.close({ success: true });
-        this.toastr.success('Pagamento realizado com sucesso!');
-        window.location.reload();
-      },
-      error: (err) => {
-        this.toastr.error('Erro ao criar a transação.');
-        console.error(err);
+    if (invoice) {
+      const amountFromInvoice = invoice.amount;
+
+      const payModel: PayInvoiceModel = {
+        accountCpf: this.paymentConfirmationForm.value.cpf,
+        payValue: this.paymentConfirmationForm.value.paymentValue,
+      };
+
+      const storageCpf = sessionStorage.getItem('cpf');
+      if (storageCpf !== payModel.accountCpf) {
+        this.toastr.error('CPF colocado não condiz com o CPF da conta');
+        return undefined;
       }
-    });
+
+      if (payModel.payValue > amountFromInvoice) {
+        this.toastr.error(
+          'O valor não pode ultrapassar o valor total da fatura.'
+        );
+        return;
+      }
+
+      this.popUpInvoiceService.payInvoice(payModel).subscribe({
+        next: (response) => {
+          this.dialogRef.close({ success: true });
+          this.toastr.success('Pagamento realizado com sucesso!');
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
+        },
+        error: (err) => {
+          this.toastr.error('Erro ao criar a transação.');
+          console.error(err);
+        },
+      });
+    } else {
+      this.toastr.error('Fatura não encontrada.');
+    }
+  }
+
+  async loadInvoiceByCpf(): Promise<Invoice | undefined> {
+    const storageCpf = sessionStorage.getItem('cpf');
+    if (storageCpf) {
+      try {
+        const data = await this.invoiceDetailsService
+          .getInvoiceByCpf(storageCpf)
+          .toPromise();
+        console.log('Invoice search completed.');
+        return data;
+      } catch (error) {
+        this.error = 'Invoice not found or request error.';
+        console.error(error);
+        return undefined;
+      }
+    } else {
+      this.handleMissingCpf();
+      return undefined;
+    }
+  }
+
+  private handleMissingCpf(): void {
+    console.error('CPF not found in sessionStorage.');
+    this.error = 'CPF not found.';
+  }
+
+  formatCurrency() {
+    let inputValue =
+      this.paymentConfirmationForm.get('paymentValue')?.value || '0.00';
+
+    inputValue = inputValue.replace(/\D/g, '').replace(',', '.');
+
+    while (inputValue.length < 3) {
+      inputValue = '0' + inputValue;
+    }
+
+    const integerPart = inputValue.slice(0, inputValue.length - 2);
+    const decimalPart = inputValue.slice(inputValue.length - 2);
+
+    const formattedValue = `${parseInt(integerPart, 10)}.${decimalPart}`;
+
+    this.paymentConfirmationForm
+      .get('paymentValue')
+      ?.setValue(formattedValue, { emitEvent: false });
   }
 }
