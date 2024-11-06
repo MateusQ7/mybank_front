2120b570a4c34ced8637de876e322c2b9687cd42
Dante <danted2300@gmail.com>
2024-11-06 15:54:06 -0300

    feat: Add new erro response for login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterModule, ActivatedRoute } from '@angular/router';
 import { FormatedFormLogin } from '../../shared/models/formatedFormLogin';
 import { LoginService } from './login.service';
@@ -12,7 +18,7 @@ import { ToastrService } from 'ngx-toastr';
   imports: [RouterModule, FormsModule, ReactiveFormsModule],
   providers: [LoginService, HttpClient],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   form!: FormGroup;
@@ -24,15 +30,15 @@ export class LoginComponent implements OnInit {
     private readonly loginService: LoginService,
     private readonly toastr: ToastrService,
     private readonly route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       email: ['', [Validators.email, Validators.required]],
-      password: ['', [Validators.minLength(6), Validators.required]]
+      password: ['', [Validators.required]],
     });
 
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       const successMessage = params['successMessage'];
       if (successMessage) {
         this.toastr.success(successMessage);
@@ -40,13 +46,12 @@ export class LoginComponent implements OnInit {
     });
 
     this.userName = sessionStorage.getItem('userName') ?? 'Não disponível';
-
   }
 
   submit() {
     const formatedFormLogin: FormatedFormLogin = {
       email: this.form.value.email,
-      password: this.form.value.password
+      password: this.form.value.password,
     };
 
     if (this.form.valid) {
@@ -58,27 +63,43 @@ export class LoginComponent implements OnInit {
             sessionStorage.setItem('name', res.name);
             sessionStorage.setItem('cpf', res.cpf);
             sessionStorage.setItem('invoice-id', res.invoiceId);
-            this.toastr.success(`Seja bem-vindo! ${this.userName}, ao myBank`, '', {
-              timeOut: 2500,  // Duração em milissegundos (5000 ms = 5 segundos)
-            });
+            this.toastr.success(
+              `Seja bem-vindo! ${this.userName}, ao myBank`,
+              '',
+              {
+                timeOut: 2500,
+              }
+            );
             this.goToHome();
           } else {
             console.error('Token ou nome ausente na resposta:', res);
-            this.toastr.error("Erro ao processar a resposta do login.");
+            this.toastr.error('Erro ao processar a resposta do login.');
           }
         },
         error: (error) => {
           console.error('Erro ao fazer login:', error);
-          this.toastr.error("Erro ao fazer login.");
+          //this.toastr.error('Erro ao fazer login.');
+          let e = error;
+          console.log(e);
+
+          switch (true) {
+            case e.error.message.includes('User not Found!'):
+              this.toastr.error('Email não encotrado');
+              break;
+
+            case e.error.message.includes('Incorrect Password!'):
+              this.toastr.error('Senha incorreta');
+              break;
+          }
         },
         complete: () => {
           console.log('Operação de login concluída.');
           this.form.reset();
-        }
+        },
       });
     } else {
       console.warn('Formulário inválido:', this.form.errors);
-      this.toastr.warning("Por favor, preencha todos os campos corretamente.");
+      this.toastr.warning('Por favor, preencha todos os campos corretamente.');
     }
   }
 
